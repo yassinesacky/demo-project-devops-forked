982c45f01d221c6e69599d6e721edb5719835215
AdamB25 <barsialtunit@gmail.com>
2025-01-28 15:19:54 -0500

    master setup

--- src/test/java/com/demo/utilities/Driver.java
+++ src/test/java/com/demo/utilities/Driver.java
@@ -1,5 +1,7 @@
 package com.demo.utilities;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,90 +19,98 @@ public class Driver {
     private Driver(){}
 
 
-    private static WebDriver driver; // default value = null
+
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
     public static WebDriver getDriver(){
-        if(driver == null){
+        if(driverPool.get() == null){
             String browserType = System.getProperty("browser") !=null?System.getProperty("browser"): ConfigurationReader.getProperty("browser");
+            String gridUrl = System.getProperty("GRID_URL")!=null?ConfigurationReader.getProperty("local.grid"): ConfigurationReader.getProperty("grid.url");
             switch (browserType){
-                case "chrome":
-                    driver=new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                    break;
-                case "chrome-headless":
+                case "chrome" -> {
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--start-maximized");
+                    options.addArguments("--remote-allow-origins=*");
+                    driverPool.set(new ChromeDriver(options));
+                }
+                case "chrome-headless" -> {
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("--headless");
-                    options.addArguments("--no-sandbox");
-                    options.addArguments("--disable-dev-shm-usage");
-                    driver=new ChromeDriver(options);
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                    break;
-                case "firefox":
-                    driver=new FirefoxDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                    break;
-                case "firefox-headless":
-                    FirefoxOptions firefoxOptions = new FirefoxOptions();
-                    firefoxOptions.addArguments("--headless");
-                    driver=new FirefoxDriver(firefoxOptions);
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                    break;
-                case "remote-chrome":
+                    driverPool.set(new ChromeDriver(options));
+                }
+                case "firefox" -> {
+                    FirefoxOptions options = new FirefoxOptions();
+                    WebDriver firefoxDriver = new FirefoxDriver(options);
+                    firefoxDriver.manage().window().fullscreen();
+                    driverPool.set(firefoxDriver);
+                }
+                case "firefox-hadless" -> {
+                    FirefoxOptions options = new FirefoxOptions();
+                    options.addArguments("--headless");
+                    driverPool.set(new FirefoxDriver(options));
+                }
+                case "remote-chrome"->{
                     DesiredCapabilities capabilities = new DesiredCapabilities();
                     capabilities.setBrowserName("chrome");
 
                     try {
-                         driver = new RemoteWebDriver(new URL(ConfigurationReader.getProperty("local.grid")+"/wd/hub"), capabilities);
+                   driverPool.set(new RemoteWebDriver(new URL(ConfigurationReader.getProperty("local.grid")+"/wd/hub"), capabilities));
                     } catch (MalformedURLException e) {
 
                     }
-                    break;
 
-                case "remote-firefox":
+                }
+
+
+                case "remote-firefox"->{
                     DesiredCapabilities firefox = new DesiredCapabilities();
                     firefox.setBrowserName("firefox");
 
                     try {
-                        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefox);
+                        driverPool.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefox));
                     } catch (MalformedURLException e) {
 
                     }
-                    break;
+
+                }
 
 
-                case "remote-chrome-aws":
+
+                case "remote-chrome-aws"->{
                     ChromeOptions remoteOptions = new ChromeOptions();
                     remoteOptions.addArguments("--start-maximized");
                     //remoteOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
                     try {
-                        driver = new RemoteWebDriver(new URL("http://54.89.147.119:4444/wd/hub"), remoteOptions);
+                        driverPool.set(new RemoteWebDriver(new URL(gridUrl+"/wd/hub"), remoteOptions));
                     } catch (MalformedURLException e) {
 
                     }
-                    break;
 
-                case "remote-firefox-aws":
+                }
+
+
+                case "remote-firefox-aws"->{
                     FirefoxOptions firefoxOptions1 = new FirefoxOptions();
                     firefoxOptions1.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
                     try {
-                        driver = new RemoteWebDriver(new URL("http://54.89.147.119:4444/wd/hub"), firefoxOptions1);
+                        driverPool.set( new RemoteWebDriver(new URL(gridUrl+"/wd/hub"), firefoxOptions1));
                     } catch (MalformedURLException e) {
 
                     }
-                    break;
+
+                }
+
 
             }
         }
-        return driver;
+        return driverPool.get();
 
     }
 
     public static void closeDriver(){
-        if (driver!=null){
-            driver.quit();
-            driver=null;
+        if (driverPool.get()!=null){
+            driverPool.get().quit();
+            driverPool.remove();
         }
     }
 
