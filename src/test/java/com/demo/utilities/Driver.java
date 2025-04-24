850179c7e4b46f7706040f7fc06dedf58c03d0d1
AdamB25 <barsialtunit@gmail.com>
2025-04-24 12:47:00 -0400

    driver is updated

--- src/test/java/com/demo/utilities/Driver.java
+++ src/test/java/com/demo/utilities/Driver.java
@@ -25,7 +25,8 @@ public class Driver {
     public static WebDriver getDriver(){
         if(driverPool.get() == null){
             String browserType = System.getProperty("browser") !=null?System.getProperty("browser"): ConfigurationReader.getProperty("browser");
-            String gridUrl = System.getProperty("GRID_URL")!=null?ConfigurationReader.getProperty("local.grid"): ConfigurationReader.getProperty("grid.url");
+           // String gridUrl = System.getProperty("GRID_URL")!=null?ConfigurationReader.getProperty("local.grid"): ConfigurationReader.getProperty("grid.url");
+            String gridUrl = ConfigurationReader.getProperty("local.grid");
             switch (browserType){
                 case "chrome" -> {
                     ChromeOptions options = new ChromeOptions();
